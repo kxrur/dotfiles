c030c647c78023560ea4d00936e71c0633b5f139
kxrur <kxrury@gmail.com>
2025-02-17 10:11:47 -0500

    update ags config

---
+++ ags/ags/modules/onscreenkeyboard/main.js
@@ -0,0 +1,12 @@
+import PopupWindow from "../.widgethacks/popupwindow.js";
+import OnScreenKeyboard from "./onscreenkeyboard.js";
+
+export default (id) =>
+  PopupWindow({
+    monitor: id,
+    anchor: ["top", "right"],
+    name: `osk${id}`,
+    showClassName: "osk-show",
+    hideClassName: "osk-hide",
+    child: OnScreenKeyboard({ id: id }),
+  });
