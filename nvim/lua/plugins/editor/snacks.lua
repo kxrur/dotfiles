c2fb0c2c0c6885868807ea52834f0b8f0f2f5734
kxrur <kxrury@gmail.com>
2025-03-18 20:28:20 -0400

    updated lazyvim - cool stuff!!

--- nvim/lua/plugins/editor/snacks.lua
+++ nvim/lua/plugins/editor/snacks.lua
@@ -12,5 +12,22 @@ return {
         position = "float",
       },
     },
+    explorer = {
+      -- your explorer configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    picker = {
+      sources = {
+        explorer = {
+          -- your explorer picker configuration comes here
+          -- or leave it empty to use the default settings
+          layout = {
+            hidden = { "input" },
+            auto_hide = { "input" },
+          },
+        },
+      },
+    },
   },
 }
